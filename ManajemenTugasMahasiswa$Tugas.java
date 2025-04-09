9e680db392721fb3cfb0cb391c0eb5b26e2129bf
iwanikhairina <131975691+iwanikhairina@users.noreply.github.com>
2025-04-10 00:08:45 +0700

    Create ManajemenTugasMahasiswa$Tugas.java

---
+++ ManajemenTugasMahasiswa$Tugas.java
@@ -0,0 +1,15 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+class ManajemenTugasMahasiswa$Tugas {
+   String nama;
+   String deskripsi;
+   String deadline;
+   String kategori;
+
+   ManajemenTugasMahasiswa$Tugas(ManajemenTugasMahasiswa var1, String var2, String var3, String var4, String var5) {
+      this.this$0 = var1;
+      this.nama = var2;
+      this.deskripsi = var3;
+      this.deadline = var4;
+      this.kategori = var5;
+   }
+}
